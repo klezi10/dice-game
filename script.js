58024e9defcc57ca714ab1857b60496c56d40b59
klezi10 <74952593+klezi10@users.noreply.github.com>
2021-12-02 10:59:08 +0700

    adding scores

--- script.js
+++ script.js
@@ -17,11 +17,15 @@ function rollDice() {
 
   if (player1Turn) {
     player1Dice.textContent = randomNumber;
+    player1Score += randomNumber;
+    player1Scoreboard.textContent = player1Score;
     player1Dice.classList.remove('active');
     player2Dice.classList.add('active');
     message.textContent = 'Player 2 Turn';
   } else if (player1Turn === false) {
     player2Dice.textContent = randomNumber;
+    player2Score += randomNumber;
+    player2Scoreboard.textContent = player2Score;
     player2Dice.classList.remove('active');
     player1Dice.classList.add('active');
     message.textContent = 'Player 1 Turn';
