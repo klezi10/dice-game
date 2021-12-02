fe997b658cd90109ddf05c8d0599f3ba00ae3f3c
klezi10 <74952593+klezi10@users.noreply.github.com>
2021-12-02 10:34:29 +0700

    set up dice logic

---
+++ script.js
@@ -0,0 +1,30 @@
+let player1Score = 0;
+let player2Score = 0;
+let player1Turn = true;
+
+const message = document.getElementById('message');
+const player1Scoreboard = document.getElementById('player1Scoreboard');
+const player2Scoreboard = document.getElementById('player2Scoreboard');
+const player1Dice = document.getElementById('player1Dice');
+const player2Dice = document.getElementById('player2Dice');
+const rollBtn = document.getElementById('rollBtn');
+const resetBtn = document.getElementById('resetBtn');
+
+rollBtn.addEventListener('click', rollDice);
+
+function rollDice() {
+  let randomNumber = Math.floor(Math.random() * 6) + 1;
+
+  if (player1Turn) {
+    player1Dice.textContent = randomNumber;
+    player1Dice.classList.remove('active');
+    player2Dice.classList.add('active');
+    message.textContent = 'Player 2 Turn';
+  } else if (player1Turn === false) {
+    player2Dice.textContent = randomNumber;
+    player2Dice.classList.remove('active');
+    player1Dice.classList.add('active');
+    message.textContent = 'Player 1 Turn';
+  }
+  player1Turn = !player1Turn;
+}
